da1df8866c661378101316f956076f134b5648f5
Norman Andrians <65523690+norman-andrians@users.noreply.github.com>
2023-06-22 22:58:41 +0700

    complete adding base wallpaper

---
+++ assets/scripts/app.js
@@ -0,0 +1,30 @@
+const rootvar = document.querySelector(":root");
+
+const clock = document.getElementById("time-clock");
+const background = document.querySelector(".main-background");
+
+function getTime() {
+    const date = new Date();
+
+    let h = date.getHours();
+    let m = date.getMinutes();
+
+    h = h < 10 ? "0" + h : h;
+    m = m < 10 ? "0" + m : m;
+
+    return `${h}:${m}`
+}
+
+window.addEventListener('mousemove', (e) => {
+    const decrease = 20;
+    const scale = parseFloat(getComputedStyle(rootvar).getPropertyValue('--background-scale'));
+
+    const x = ((window.innerWidth / 2) - e.pageX) / decrease;
+    const y = ((window.innerHeight / 2) - e.pageY) / decrease;
+
+    background.style.transform = `translate(${x}px, ${y}px) scale(${scale})`;
+});
+
+setInterval(() => {
+    clock.textContent = getTime();
+}, 100);
