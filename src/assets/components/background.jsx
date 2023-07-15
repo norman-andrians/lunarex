61fa4241efe99105ed48fcfcc15e0634028866c4
Norman Andrians <65523690+norman-andrians@users.noreply.github.com>
2023-07-16 02:07:34 +0700

    refactoring part 1

---
+++ src/assets/components/background.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Background() {
+    return (
+        <div className="main-background bg-img"><div className="bg-blur"></div></div>
+    );
+}
+
+export default Background;
